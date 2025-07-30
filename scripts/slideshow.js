7c2de95d2e4a0eb3120ebec08303809b4778f01c
nwillett-neumont <nwillett@student.neumont.edu>
2025-07-30 17:21:47 -0600

    Added slideshow functionality for the main page and started styles.

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -2,10 +2,41 @@
 const slideshowDisplay = document.getElementById('slideshow');
 
 // Variables
-
+let count = 0;
 // Event Listeners
+
+// Run
+changeImage();
 
 // Functions
 function changeImage() {
-    
+    setInterval(function () {
+        document.getElementById('slideshow').src = setImage();
+    }, 8000);
+}
+
+function randomImage() {
+    let rand = Math.floor(Math.random() * 3) + 1;
+
+    switch(rand) {
+        case 1:
+            return "./assets/images/slideshow/dylan.png";
+        case 2:
+            return "./assets/images/slideshow/vscode.png";
+        case 3:
+            return "./assets/images/slideshow/dark-souls.jpg"
+    }
+}
+
+function setImage() {
+    if (count == 3) count = 0;
+    count++;
+    switch(count) {
+        case 1:
+            return "./assets/images/slideshow/dylan.png";
+        case 2:
+            return "./assets/images/slideshow/vscode.png";
+        case 3:
+            return "./assets/images/slideshow/dark-souls.jpg"
+    } 
 }
